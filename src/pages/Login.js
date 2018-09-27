003cb26d3e978ac4530c9dab49d86700a9243bf3
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2018-09-27 16:56:20 -0300

    Quarta alteração

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,8 +9,6 @@ import { login } from "../store/ducks/auth";
 
 import { withFormik } from "formik";
 
-import Footer from "../components/Footer";
-
 import LogoImg from "../assets/img/tropperstorm.png";
 
 class LoginForm extends Component {
@@ -22,88 +20,81 @@ class LoginForm extends Component {
 
     const { values, handleChange, handleSubmit, isSubmitting } = this.props;
     return (
-      <div className="container-fluid block-margin">
-        <div className="row">
-          <div className="col-xl-4 col-lg-5 col-md-6 col-sm-8 ml-auto mr-auto">
-            <form onSubmit={handleSubmit}>
-              <div className="card">
-                <h4 className="card-header bg-secondary mb-3 text-white text-center">
-                  <img
-                    src={LogoImg}
-                    alt={StarWarsLogo}
-                    style={{ width: "50px", height: "auto" }}
-                  />{" "}
-                  <FormattedMessage id="StarWars" />
-                </h4>
-                <div className="card-body">
-                  <h4 className="text-center">
-                    <FormattedMessage id="welcome" />
-                  </h4>
-                  <p className="text-center">
-                    <FormattedMessage id="darkside" />
-                  </p>
+      <form className="form-login" onSubmit={handleSubmit}>
+        <div className="card">
+          <h4 className="card-header bg-secondary mb-3 text-white text-center">
+            <img
+              src={LogoImg}
+              alt={StarWarsLogo}
+              style={{ width: "50px", height: "auto" }}
+            />{" "}
+            <FormattedMessage id="StarWars" />
+          </h4>
+          <div className="card-body">
+            <h4 className="text-center">
+              <FormattedMessage id="welcome" />
+            </h4>
+            <p className="text-center">
+              <FormattedMessage id="darkside" />
+            </p>
 
-                  <div className="form">
-                    <div className="input-group">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text">
-                          <i className="material-icons">face</i>
-                        </span>
-                      </div>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="username"
-                        placeholder={username}
-                        required
-                        maxLength="50"
-                        value={values.username}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="input-group">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text">
-                          <i className="material-icons">lock</i>
-                        </span>
-                      </div>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        placeholder={password}
-                        required
-                        maxLength="15"
-                        value={values.password}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div>
-                      <br />
-                      <FormattedMessage id="withoutAccount" />
-                      <Link to="/signup">
-                        <FormattedMessage id="createAccount" />
-                      </Link>
-                    </div>
-                  </div>
+            <div className="form">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="material-icons">face</i>
+                  </span>
                 </div>
-                <div className="card-footer">
-                  <div>
-                    <button
-                      type="submit"
-                      className="btn btn-dark btn-lg mx-auto d-block btn-block"
-                      disabled={isSubmitting}
-                    >
-                      <FormattedMessage id="enter" />
-                    </button>
-                  </div>
-                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  placeholder={username}
+                  required
+                  maxLength="50"
+                  value={values.username}
+                  onChange={handleChange}
+                />
               </div>
-            </form>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="material-icons">lock</i>
+                  </span>
+                </div>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder={password}
+                  required
+                  maxLength="15"
+                  value={values.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <br />
+                <FormattedMessage id="withoutAccount" />
+                <Link to="/signup">
+                  <FormattedMessage id="createAccount" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="card-footer">
+            <div>
+              <button
+                type="submit"
+                className="btn btn-dark btn-lg mx-auto d-block btn-block"
+                disabled={isSubmitting}
+              >
+                <FormattedMessage id="enter" />
+              </button>
+            </div>
           </div>
         </div>
-        <Footer />
-      </div>
+      </form>
     );
   }
 }
