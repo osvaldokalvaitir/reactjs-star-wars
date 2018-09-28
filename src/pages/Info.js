7f799b9c912db76c7a2847626f3b87f98c690029
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2018-09-28 17:07:42 -0300

    Convertendo Json para Lista

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import { connect } from "react-redux";
-
 import { FormattedMessage } from "react-intl";
 
-import ListItemInfo from "../components/ListItemInfo";
+import ListInfo from "../components/ListInfo";
 
 class Info extends Component {
-  state = {
-    obj: []
-  };
-
   componentDidMount() {
-    const id = this.props.match.params.id;
-    const obj = this.props.peoples[id];
-    this.setState({ obj });
+    console.log(window.location.href)
   }
 
   render() {
     return (
-      <div className="card block-height block-width bg-secondary">
+      <div className="card bg-secondary block-height block-width">
         <div className="btn-group block-width bg-dark">
           <Link className="btn btn-dark" to="/">
             <i className="material-icons align-bottom">arrow_back</i>
@@ -28,29 +20,10 @@ class Info extends Component {
             <FormattedMessage id="back" />
           </Link>
         </div>
-        <ul className="list-group">
-          <li className="list-group-item list-group-item-dark">
-            {
-              JSON.stringify(this.state.obj)
-            // this.state.obj
-            //      .map((item, id) => <ListItemInfo item={item} key={id} />)
-            }
-          </li>
-        </ul>
+        <ListInfo resource={this.props.match.params.id} id={this.props.match.params.id} />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  peoples: state.peoples,
-  planets: state.planets,
-  species: state.species,
-  starships: state.starships,
-  vehicles: state.vehicles
-});
-
-export default connect(
-  mapStateToProps,
-  null
-)(Info);
+export default Info;
